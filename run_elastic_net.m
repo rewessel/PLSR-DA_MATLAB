4461aeea28a20e3226633f078fd5073db834e93d
re8sb <225431259+rewessel@users.noreply.github.com>
2025-08-13 14:15:11 -0400

    Merge run_elastic_net.m function
    Copied run_elastic_net.m function from Shay's repo into this repo so all PLSR/DA functions (plus LASSO) are in the same place.

---
+++ run_elastic_net.m
@@ -0,0 +1,26 @@
+function features_filt = run_elastic_net(X, Y, method, alpha, reps, stability, cv_reps)
+arguments
+    X % The input matrix for future PLSDA/R. No normalization performed within this function - z-score or log normalize prior if desired
+    Y % The response matrix for future PLSDA/R
+    method % either minMSE or 1SE, defines the method to be used when selecting the best feature set
+    alpha = 1 % A value between 0 and 1, with 0 being fully elastic net and 1 being regular LASSO
+    reps = 500 % The number of repetitions to perform
+    stability = 0.8 % The percentage of repetitions a feature must be selected in to be included in the final feature set
+    cv_reps = 5 % cross validation repetitions for the internal lasso function - use the same value as for the final PLSDA/R model
+end
+X_z = table2array(X);
+varNames = X.Properties.VariableNames;
+elastic_feat_names = {};
+for n = 1:reps
+    [b,fitInfo] = lasso(X_z,Y(:,1),'CV',cv_reps,'Alpha',alpha);
+    if (strcmp(method, 'minMSE'))
+        [~,idx] = min(fitInfo.MSE);
+    elseif (strcmp(method, '1SE'))
+        idx=max(fitInfo.Index1SE,1);
+    end
+    temp_name = varNames(any(b(:,idx),2));
+    elastic_feat_names = [elastic_feat_names, temp_name];
+end
+[feature_counts, elastic_feats] = groupcounts((elastic_feat_names)');
+features_filt = elastic_feats(feature_counts > reps*stability);
+end
